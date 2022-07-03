0c025bfb1289ccfd9a1187f1224e2b7f33fa0718
kiketion <kiketionje@gmail.com>
2022-07-03 18:21:33 +0300

    configuring express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('hello jack!');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello Jack!'));
+
+app.listen(3000);
+console.log('On port:', 3000);
