022854ebe834ca346bf92010878d580183571c67
kiketion <kiketionje@gmail.com>
2022-07-06 18:13:56 +0300

    updt2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import express from 'express';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello Jack!'));
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(join(__dirname, 'pages'));
+
+app.set('views', join(__dirname, 'pages'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('index'));
+
+app.get('/about', (req, res) => res.render('about'));
+
+app.get('/contact', (req, res) => res.render('contact'));
 
 app.listen(3000);
 console.log('On port:', 3000);
